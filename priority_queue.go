7e13c0fffd7a57f6944df7ec93394092f62a7058
Simoncqk <cqk0100@gmail.com>
2019-01-17 16:02:06 +0800

    add some comments of priority queue.

--- priority_queue.go
+++ priority_queue.go
@@ -7,6 +7,7 @@ type Element struct {
 	Priority int
 }
 
+// LessCompFunc for comparing two Element instance.
 func elementLessComp(a, b interface{}) bool {
 	return a.(*Element).Priority < b.(*Element).Priority
 }
@@ -18,22 +19,30 @@ func NewPriorityQueue() *PriorityQueue {
 	}
 }
 
+// PriorityQueue implementation, meanwhile it's a max heap.
 type PriorityQueue struct {
 	minHeap *Heap
 }
 
+// Push add a new element with its priority into priority queue,
+// since queue holds a minHeap, let the internal priority be the
+// negative priority of the origin, so min-max relationship is
+// reversed.
 func (q *PriorityQueue) Push(elem interface{}, priority int) {
 	q.minHeap.Push(&Element{Value: elem, Priority: -priority})
 }
 
+// Peek return the element with max priority.
 func (q *PriorityQueue) Peek() *Element {
 	if q.Size() == 0 {
 		return nil
 	}
 	top := q.minHeap.Top().(*Element)
+	// recover its real priority.
 	return &Element{Value: top.Value, Priority: -top.Priority}
 }
 
+// Pop remove the element with max priority and return it.
 func (q *PriorityQueue) Pop() *Element {
 	if q.Size() == 0 {
 		return nil
